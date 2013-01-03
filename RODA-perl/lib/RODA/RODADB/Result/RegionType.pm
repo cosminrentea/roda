c6975a87d6fa3c6386d6234cf3440b27005c6ac0
Sorin Milutinovici <sorin@contentlogic.ro>
2013-01-03 14:16:01 +0000

    modificat id-ul in serial

--- RODA-perl/lib/RODA/RODADB/Result/RegionType.pm
+++ RODA-perl/lib/RODA/RODADB/Result/RegionType.pm
@@ -42,28 +42,33 @@ __PACKAGE__->table("region_type");
 
 =head1 ACCESSORS
 
-=head2 id
-
-  data_type: 'integer'
-  is_nullable: 0
-
-Identificatorul tipului de regiune
-
 =head2 name
 
   data_type: 'varchar'
   is_nullable: 0
   size: 150
 
+=head2 id
+
+  data_type: 'integer'
+  is_auto_increment: 1
+  is_nullable: 0
+  sequence: 'region_type_id_seq'
+
 Denumirea tipului de regiune
 
 =cut
 
 __PACKAGE__->add_columns(
-  "id",
-  { data_type => "integer", is_nullable => 0 },
   "name",
   { data_type => "varchar", is_nullable => 0, size => 150 },
+  "id",
+  {
+    data_type         => "integer",
+    is_auto_increment => 1,
+    is_nullable       => 0,
+    sequence          => "region_type_id_seq",
+  },
 );
 
 =head1 PRIMARY KEY
@@ -96,8 +101,8 @@ __PACKAGE__->has_many(
 );
 
 
-# Created by DBIx::Class::Schema::Loader v0.07012 @ 2012-12-19 19:21:26
-# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:Cxl08y+9cz0MlKiCPZjiDg
+# Created by DBIx::Class::Schema::Loader v0.07012 @ 2013-01-03 14:59:56
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:A3YXEL+z71ufyI8arBv+ZQ
 
 
 # You can replace this text with custom code or comments, and it will be preserved on regeneration

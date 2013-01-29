8b554d13729f63428b798251f457a2c41766eee0
Cosmin Rentea <cosmin.rentea@gmail.com>
2013-01-29 16:19:26 +0000

    roda-create.sh: minor change (comma exclusion in regexp) roda-drop.sh: drop also all sequences, not only all tables

--- dbwrench2-files/roda-drop.sh
+++ dbwrench2-files/roda-drop.sh
@@ -7,9 +7,15 @@ TEMPSCRIPT=`mktemp`
 echo "${TEMPSCRIPT}"
 
 # get a list the "DROP" commands - for all the tables
-psql -w -t -o ${TEMPSCRIPT} -h ${RODA_HOST}  -c "select 'drop table if exists \"' || tablename || '\" cascade;' from pg_tables where schemaname = 'public';" ${RODA_DB} ${RODA_USER}
+psql -w -t -o ${TEMPSCRIPT} -h ${RODA_HOST}  -c "SELECT 'DROP TABLE IF EXISTS \"' || tablename || '\" cascade;' from pg_tables where schemaname = 'public';" ${RODA_DB} ${RODA_USER}
 
-# execute the DROPs
+# execute all the DROP TABLE commands
+psql -w -f "${TEMPSCRIPT}" -h ${RODA_HOST} ${RODA_DB} ${RODA_USER}
+
+# get a list the "DROP SEQUENCE" commands - for all the sequences in the database
+psql -w -t -o ${TEMPSCRIPT} -h ${RODA_HOST}  -c "SELECT 'DROP SEQUENCE \"' || relname || '\";' FROM pg_class WHERE  relkind = 'S';" ${RODA_DB} ${RODA_USER}
+
+# execute all the DROP SEQUENCE commands
 psql -w -f "${TEMPSCRIPT}" -h ${RODA_HOST} ${RODA_DB} ${RODA_USER}
 
 # remove temporary file

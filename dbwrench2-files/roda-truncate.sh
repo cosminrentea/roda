5f1a4399a2e88b8a9626059fb84c17d44bdf5e4f
Cosmin Rentea <cosmin.rentea@gmail.com>
2013-02-05 23:54:31 +0000

    added script to remove all rows from all DB tables (uses "TRUNCATE") it is the "antidote" of the seeddb Perl script

---
+++ dbwrench2-files/roda-truncate.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+source database.properties
+
+# create temporary file for an SQL script
+TEMPSCRIPT=`mktemp`
+echo "${TEMPSCRIPT}"
+
+# get a list of the "TRUNCATE" commands - for all the tables
+psql -a -w -t -o ${TEMPSCRIPT} -h ${RODA_HOST}  -c "SELECT 'TRUNCATE TABLE \"' || tablename || '\" RESTART IDENTITY CASCADE;' from pg_tables where schemaname = 'public';" ${RODA_DB} ${RODA_USER}
+
+# execute all the DROP TABLE commands
+psql -a -w -f "${TEMPSCRIPT}" -h ${RODA_HOST} ${RODA_DB} ${RODA_USER}
+
+# remove temporary file
+rm "${TEMPSCRIPT}"

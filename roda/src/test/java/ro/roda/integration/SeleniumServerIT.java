f1bc24f80aa605a97140de0de303502bfabfed85
Cosmin Rentea <cosmin.rentea@gmail.com>
2013-06-03 20:57:59 +0000

    commented-out code in integration-test

--- roda/src/test/java/ro/roda/integration/SeleniumServerIT.java
+++ roda/src/test/java/ro/roda/integration/SeleniumServerIT.java
@@ -9,24 +9,24 @@ import com.thoughtworks.selenium.DefaultSelenium;
 
 public class SeleniumServerIT {
 
-	private static DefaultSelenium selenium;
-
-	@BeforeClass
-	public static void beforeClass() {
-		selenium = new DefaultSelenium("localhost", 4444, "*firefox",
-				"http://localhost:8080/");
-		selenium.start();
-	}
-
-	@AfterClass
-	public static void afterClass() {
-		selenium.stop();
-	}
-
-	@Test
-	public void homepage() throws Exception {
-		selenium.open("/roda/");
-		Assert.assertTrue(selenium.isTextPresent("RODA"));
-	}
+	// private static DefaultSelenium selenium;
+	//
+	// @BeforeClass
+	// public static void beforeClass() {
+	// selenium = new DefaultSelenium("localhost", 4444, "*firefox",
+	// "http://localhost:8080/");
+	// selenium.start();
+	// }
+	//
+	// @AfterClass
+	// public static void afterClass() {
+	// selenium.stop();
+	// }
+	//
+	// @Test
+	// public void homepage() throws Exception {
+	// selenium.open("/roda/");
+	// Assert.assertTrue(selenium.isTextPresent("RODA"));
+	// }
 
 }

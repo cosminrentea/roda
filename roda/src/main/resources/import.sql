361c672c1d2b15c89e920e11bb104089b2229189
Cosmin Rentea <cosmin.rentea@gmail.com>
2013-07-17 12:23:39 +0000

    removed/commented some test data in SQL + CSVs

--- roda/src/main/resources/import.sql
+++ roda/src/main/resources/import.sql
@@ -1,2 +1,2 @@
-insert into keyword(name) values ('keyword-1');
-insert into keyword(name) values ('keyword-2');
+-- insert into keyword(name) values ('keyword-1');
+-- insert into keyword(name) values ('keyword-2');

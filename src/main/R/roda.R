5dc39eab56d59d09f8ca830a2e3ec1cf0fbcc166
Adrian Dusa <dusa.adrian@gmail.com>
2014-08-12 16:43:11 +0300

    Mici modificari la primul API de R
    Am inlocuit "Foarte rau" cu "F.rau"

--- src/main/R/roda.R
+++ src/main/R/roda.R
@@ -3,4 +3,4 @@ table(c(1,3,1,2,4,1,5,2,3), c(2,1,1,2,1,1,1,2,1))
 
 myfunction(list(c(1,3,1,2,4,1,5,2,3), c(2,1,1,2,1,1,1,2,1)),
            list(c(1,2,3,4,5), c(1,2)),
-           list(c("F.bine", "Bine", "Asa si asa", "Rau", "Foarte rau"), c("Urban", "Rural")))
+           list(c("F.bine", "Bine", "Asa si asa", "Rau", "F.rau"), c("Urban", "Rural")))

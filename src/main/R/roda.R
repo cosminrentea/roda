3dd5027a73c533a148e4f23ff61d372b1bfab40c
Adrian Dusa <dusa.adrian@gmail.com>
2014-08-12 16:42:07 +0300

    Update roda.R

---
+++ src/main/R/roda.R
@@ -0,0 +1,6 @@
+# ambii vectori (variabile) trebuie sa aiba aceeasi lungime
+table(c(1,3,1,2,4,1,5,2,3), c(2,1,1,2,1,1,1,2,1))
+
+myfunction(list(c(1,3,1,2,4,1,5,2,3), c(2,1,1,2,1,1,1,2,1)),
+           list(c(1,2,3,4,5), c(1,2)),
+           list(c("F.bine", "Bine", "Asa si asa", "Rau", "Foarte rau"), c("Urban", "Rural")))

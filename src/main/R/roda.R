55e1ea4e0c25c5da6e6d591ecfe6a3db1eac9718
Adrian Dusa <dusa.adrian@gmail.com>
2014-08-27 17:42:28 +0300

    curatare text
    Pentru moment, nu mai este nevoie nici de pachetul rjson, nici de DDIwR.

--- src/main/R/roda.R
+++ src/main/R/roda.R
@@ -1,15 +1,3 @@
-# install.packages("DDIwR", repos="http://R-Forge.R-project.org")
-
-library(rjon)
-library(DDIwR)
-
-# ambii vectori (variabile) trebuie sa aiba aceeasi lungime
-table(c(1,3,1,2,4,1,5,2,3), c(2,1,1,2,1,1,1,2,1))
-
-myfunction(list(c(1,3,1,2,4,1,5,2,3), c(2,1,1,2,1,1,1,2,1)),
-           list(c(1,2,3,4,5), c(1,2)),
-           list(c("F.bine", "Bine", "Asa si asa", "Rau", "F.rau"), c("Urban", "Rural")))
-
 
 # o variabila categoriala v1 cu 105 de observatii, dintre care 5 de missing
 

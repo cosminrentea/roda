199f5e37f0183a128c593bffecab490c58fdef51
Cosmin Rentea <cosmin.rentea@gmail.com>
2014-09-17 19:15:15 +0300

    updated scripts for running ad-hoc Tomcat

--- script/tomcat-run-production.sh
+++ script/tomcat-run-production.sh
@@ -1,1 +1,9 @@
-MAVEN_OPTS="-Xmx1024m -XX:MaxPermSize=256m" mvn -f ../pom.xml -Djava.awt.headless=true -Dspring.profiles.active=production -Dmaven.tomcat.fork=false -X -e clean package tomcat:run
+export R_HOME="/usr/lib/R"
+
+# Ubuntu Cosmin
+export LD_LIBRARY_PATH="/usr/lib/jvm/java-7-oracle/jre/lib/i386/client:/usr/lib/R/lib:/home/cosmin/R/i686-pc-linux-gnu-library/3.0/rJava/libs/:/home/cosmin/R/i686-pc-linux-gnu-library/3.0/rJava/jri"
+
+# Kubuntu Sorin
+#export LD_LIBRARY_PATH="/usr/lib/jvm/java-1.7.0-openjdk-amd64/jre/lib/amd64/server/:/usr/lib/R/lib:/usr/local/lib/R/site-library/rJava/jri:/usr/local/lib/R/site-library/rJava/libs"
+
+MAVEN_OPTS="-Xmx1024m -XX:MaxPermSize=256m" mvn -f ../pom.xml -Djava.awt.headless=true -DR_HOME=/usr/lib/R -Djava.library.path="${LD_LIBRARY_PATH}" -Dspring.profiles.active=production -Dmaven.tomcat.fork=false -X -e clean package tomcat:run

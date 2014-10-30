79370f13bb931432d7fb30a59e9491565dc3136d
Cosmin Rentea <cosmin.rentea@gmail.com>
2014-10-30 16:13:03 +0200

    data files
    closes #163
    closes #170

--- src/main/resources/import-productioninit.sql
+++ src/main/resources/import-productioninit.sql
@@ -1,10 +1,13 @@
 -- users
-insert into users(username,password,enabled) values ('admin','8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918',true);
-insert into users(username,password,enabled) values ('user','04f8996da763b7a969b1028ee3007569eaf3a635486ddab211d512c85b9df8fb',true);
-insert into users(username,password,enabled) values ('visitor','5f14f9e6d80f802a65269804f2552ef9889f2c7ccec5067214e58a1e48e0b3ff',true);
---insert into users(username,password,enabled,person_id) values ('admin','8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918',true,1);
---insert into users(username,password,enabled,person_id) values ('user','04f8996da763b7a969b1028ee3007569eaf3a635486ddab211d512c85b9df8fb',true,2);
---insert into users(username,password,enabled,person_id) values ('visitor','5f14f9e6d80f802a65269804f2552ef9889f2c7ccec5067214e58a1e48e0b3ff',true,3);
+
+-- admin password: R0daAd0rAb11
+insert into users(username,password,enabled) values ('admin','25bea7864803ca10c48b33a56ae1225d692a57fc1b5739c08b9f43420a0ff156',true);
+
+-- user password: Ab0rdab11
+insert into users(username,password,enabled) values ('user','d9a440f9f8fd4b3d8c2a7c0f3540e05435daecb6348b1dd2ba31f46bdd55630d',true);
+
+-- visitor password: V1s1t0r
+insert into users(username,password,enabled) values ('visitor','e61bda547b339685a93108103e97684a3081b5047621b2ec3bebe240d15cd8dc',true);
 
 -- user_group
 insert into user_group(groupname,enabled,description) values ('ROLE_ADMIN',true,'group for administrators');
@@ -25,4 +28,6 @@ insert into user_profile(user_id, firstname, middlename, lastname, title, saluta
 insert into user_profile(user_id, firstname, middlename, lastname, title, salutation, sex, birthdate, image, city, country, address1, phone) values (2, 'Vasile', 'V.', 'Vasilica', 'doctor', 'domnul', 'M', '12-07-1970', 'default-user-m', 'Ploiesti', 'Romania', 'str. Lalelelor nr. 25', '0745678907');
 insert into user_profile(user_id, firstname, middlename, lastname, title, salutation, sex, birthdate, image, city, country, address1, phone) values (3, 'Ion', 'T.', 'Popescu', 'doctor', 'domnul', 'M', '03-11-1975', 'default-user-m', 'Bucuresti', 'Romania', 'str. Traian nr. 48', '0729789678');
 
-
+-- global settings 
+insert into setting(name, "value") values ('baseurl','/');
+insert into setting(name, "value") values ('defaultlanguage','en');

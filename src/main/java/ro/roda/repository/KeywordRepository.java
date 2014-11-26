220c5904830709c6431dccbd848a52f2cd51c78b
Cosmin Rentea <cosmin.rentea@gmail.com>
2014-11-26 18:56:42 +0200

    ACL CSVs in each Import-Profile; API REST Keywords

--- src/main/java/ro/roda/repository/KeywordRepository.java
+++ src/main/java/ro/roda/repository/KeywordRepository.java
@@ -1,5 +1,7 @@
 package ro.roda.repository;
 
+import java.util.List;
+
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
@@ -7,5 +9,7 @@ import ro.roda.domain.Keyword;
 
 @RepositoryRestResource
 public interface KeywordRepository extends PagingAndSortingRepository<Keyword, Integer> {
+
+	List<Keyword> findByName(String name);
 
 }

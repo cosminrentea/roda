bd503a8ad0121666226496d413b5396fbf3d0973
sorinmilu <sorin@contentlogic.ro>
2014-12-15 12:20:38 +0200

    leftovers...

---
+++ src/main/webapp/RODAdmin/app/view/MainUserPanel.js
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+Ext.define('RODAdmin.view.MainUserPanel', {
+    extend: 'Ext.tab.Panel',
+    alias: 'widget.mainuserpanel',
+
+    requires: ['RODAdmin.view.dashboard.Dashboard'],
+    
+    activeTab: 0,
+
+    items: [
+//        {
+//        	xtype: 'dashboard',
+//            closable: false,
+//            iconCls: 'home',
+//            title: 'Home',
+//            layout: 'fit'
+//        }
+    ]
+});
---
+++ src/main/webapp/RODAdmin/app/view/UserViewport.js
@@ -0,0 +1,42 @@
+/**
+ * 
+ */
+Ext.define('RODAdmin.view.UserViewport', {
+    extend: 'Ext.container.Viewport',
+    alias: 'widget.userviewport',
+
+    requires: [
+        'RODAdmin.view.Header',
+        'RODAdmin.view.menu.Accordion',
+        'RODAdmin.view.MainPanel'
+    ],
+
+    layout: {
+        type: 'border'
+    },
+
+    items: [
+        {
+            xtype: 'mainmenu',
+            width: 185,
+            collapsible: true,
+            region: 'west'//,
+            //style: 'background-color: #8FB488;'
+        },
+        {
+            xtype: 'appheader',
+            region: 'north'
+        },
+        {
+            xtype: 'mainuserpanel',
+            region: 'center'
+        },
+        {
+            xtype: 'container',
+            region: 'south',
+            height: 30,
+            style: 'border-top: 1px solid #4c72a4;',
+            html: '<div id="titleHeader"><center><span style="font-size:10px;">RODA Admin</span></center></div>'
+        }
+    ]
+});

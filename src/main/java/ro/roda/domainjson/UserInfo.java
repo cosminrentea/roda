085c1cc2d8558f6918dc7c651a4ec5456713d426
letitia <letitia.v@gmail.com>
2014-12-18 00:19:24 +0200

    Controllers /adminjson/userslist and /adminjson/userinfo/<id> revised: both return the same information at the same level (no more "profile" level included); userinfo retrieves also the user messages. (Closes #219, #224)

--- src/main/java/ro/roda/domainjson/UserInfo.java
+++ src/main/java/ro/roda/domainjson/UserInfo.java
@@ -10,11 +10,7 @@ import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Configurable;
 
-import ro.roda.domain.CmsLayout;
-import ro.roda.domain.CmsLayoutGroup;
-import ro.roda.domain.CmsPage;
 import ro.roda.domain.Person;
-import ro.roda.domain.PersonLinks;
 import ro.roda.domain.UserMessage;
 import ro.roda.domain.Users;
 import ro.roda.transformer.FieldNameTransformer;
@@ -26,9 +22,8 @@ public class UserInfo extends UserList {
 	public static String toJsonArr(Collection<UserInfo> collection) {
 		JSONSerializer serializer = new JSONSerializer();
 
-		serializer.exclude("*.class", "id", "type", "firstname", "lastname", "profile.name", "profile.id",
-				"profile.type");
-		serializer.include("name", "email", "enabled", "profile", "messages");
+		serializer.exclude("*.class", "type");
+		serializer.include("messages");
 
 		serializer.transform(new FieldNameTransformer("username"), "name");
 		serializer.transform(DATE_TRANSFORMER2, Date.class);
@@ -58,69 +53,54 @@ public class UserInfo extends UserList {
 		Users profile = user;
 
 		if (user != null) {
-			Set<PersonLinks> personLinks = user.getPersonLinkss();
-			if (personLinks != null && personLinks.size() > 0) {
-				Person person = personLinks.iterator().next().getPersonId();
-				result = new UserInfo(user, person);
-				// ensure that there are no differences between the information
-				// in Person (if it exists) and the one in the UserProfile; the
-				// information in the Person table has greater priority
-				if (profile != null) {
-					profile.setLastname(person.getLname());
-					profile.setFirstname(person.getFname());
 
-					// the field mname is nullable in the table Person; if null,
-					// take the one in UserProfile
-					if (person.getMname() != null) {
-						profile.setMiddlename(person.getMname());
-					}
-				}
-			} else {
-				// use the userProfile information
-				result = new UserInfo(user);
-			}
+			result = new UserInfo(user);
+
+			// TODO decide if Person Links are still necessary
+			// Set<PersonLinks> personLinks = user.getPersonLinkss();
+			// if (personLinks != null && personLinks.size() > 0) {
+			// Person person = personLinks.iterator().next().getPersonId();
+			// result = new UserInfo(user, person);
+			// // ensure that there are no differences between the information
+			// // in Person (if it exists) and the one in the UserProfile; the
+			// // information in the Person table has greater priority
+			// if (profile != null) {
+			// profile.setLastname(person.getLname());
+			// profile.setFirstname(person.getFname());
+			//
+			// // the field mname is nullable in the table Person; if null,
+			// // take the one in UserProfile
+			// if (person.getMname() != null) {
+			// profile.setMiddlename(person.getMname());
+			// }
+			// }
+			// } else {
+			// // use the userProfile information
+			// result = new UserInfo(user);
+			// }
 
 			Set<UserMessage> userMessages = new HashSet<UserMessage>();
 
 			// set the remaining information from the profile
-			if (profile != null) {
-				result.setProfile(new UserProfileInfo(profile));
-			}
+			// if (profile != null) {
+			// result.setProfile(new UserProfileInfo(profile));
+			// }
 			result.setMessages(userMessages);
 		}
 
 		return result;
 	}
 
-	public static Set<CmsPage> getLayoutGroupPages(CmsLayoutGroup layoutGroup) {
-		Set<CmsPage> pages = new HashSet<CmsPage>();
-
-		Iterator<CmsLayout> layoutsIterator = layoutGroup.getCmsLayouts().iterator();
-
-		while (layoutsIterator.hasNext()) {
-			pages.addAll(layoutsIterator.next().getCmsPages());
-		}
-
-		Iterator<CmsLayoutGroup> layoutGroupsIterator = layoutGroup.getCmsLayoutGroups().iterator();
-
-		while (layoutGroupsIterator.hasNext()) {
-			pages.addAll(getLayoutGroupPages(layoutGroupsIterator.next()));
-		}
-
-		return pages;
-	}
-
+	// TODO decide if groups are no longer needed; in this case, remove the
+	// following field:
 	private Set<UserGroupInfo> groups;
-
-	private UserProfileInfo profile;
 
 	private Set<UserMessage> messages;
 
 	public UserInfo(Integer id, String username, String email, boolean enabled, Set<UserGroupInfo> groups,
-			UserProfileInfo profile, Set<UserMessage> messages) {
+			Set<UserMessage> messages) {
 		super(id, username, null, null, email, enabled);
 		this.groups = groups;
-		this.profile = profile;
 		this.messages = messages;
 	}
 
@@ -140,14 +120,11 @@ public class UserInfo extends UserList {
 
 	public UserInfo(Users user) {
 		super(user);
+		this.messages = user.getUserMessages();
 	}
 
 	public Set<UserGroupInfo> getGroups() {
 		return groups;
-	}
-
-	public UserProfileInfo getProfile() {
-		return profile;
 	}
 
 	public Set<UserMessage> getMessages() {
@@ -158,10 +135,6 @@ public class UserInfo extends UserList {
 		this.groups = groups;
 	}
 
-	public void setProfile(UserProfileInfo userProfile) {
-		this.profile = userProfile;
-	}
-
 	public void setMessages(Set<UserMessage> messages) {
 		this.messages = messages;
 	}
@@ -169,9 +142,8 @@ public class UserInfo extends UserList {
 	public String toJson() {
 		JSONSerializer serializer = new JSONSerializer();
 
-		serializer.exclude("*.class", "id", "type", "firstname", "lastname", "profile.name", "profile.id",
-				"profile.type");
-		serializer.include("name", "email", "enabled", "profile", "messages");
+		serializer.exclude("*.class", "type");
+		serializer.include("messages");
 
 		serializer.transform(new FieldNameTransformer("username"), "name");
 		serializer.transform(DATE_TRANSFORMER2, Date.class);
--- src/main/java/ro/roda/domainjson/UserList.java
+++ src/main/java/ro/roda/domainjson/UserList.java
@@ -2,14 +2,13 @@ package ro.roda.domainjson;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Configurable;
 
 import ro.roda.domain.Person;
-import ro.roda.domain.PersonLinks;
 import ro.roda.domain.Users;
 import ro.roda.transformer.FieldNameTransformer;
 import flexjson.JSONSerializer;
@@ -21,8 +20,11 @@ public class UserList extends JsonInfo {
 		JSONSerializer serializer = new JSONSerializer();
 
 		serializer.exclude("*.class", "type");
-		serializer.include("id", "name", "firstname", "lastname", "email", "enabled");
+		serializer.include("id", "name", "firstname", "lastname", "middlename", "email", "enabled");
+		serializer.include("title", "salutation", "image", "address1", "address2", "city", "country", "phone", "sex",
+				"birthdate");
 
+		serializer.transform(DATE_TRANSFORMER2, Date.class);
 		serializer.transform(new FieldNameTransformer("username"), "name");
 
 		return "{\"data\":" + serializer.serialize(collection) + "}";
@@ -52,16 +54,22 @@ public class UserList extends JsonInfo {
 		List<UserList> result = new ArrayList<UserList>();
 
 		if (user != null) {
-			Set<PersonLinks> personLinks = user.getPersonLinkss();
-			if (personLinks != null && personLinks.size() > 0) {
-				// TODO a user should have only one PersonLink in the database,
-				// too
-				PersonLinks personLink = personLinks.iterator().next();
-				Person person = personLink.getPersonId();
-				result.add(new UserList(user, person));
-			} else {
-				result.add(new UserList(user));
-			}
+
+			result.add(new UserList(user));
+
+			// TODO: decide if the relation with PersonLinks is still
+			// interesting; if yes, delete the line above; else, delete the
+			// commented code:
+			// Set<PersonLinks> personLinks = user.getPersonLinkss();
+			// if (personLinks != null && personLinks.size() > 0) {
+			// // TODO a user should have only one PersonLink in the database,
+			// // too
+			// PersonLinks personLink = personLinks.iterator().next();
+			// Person person = personLink.getPersonId();
+			// result.add(new UserList(user, person));
+			// } else {
+			// result.add(new UserList(user));
+			// }
 			return result;
 		}
 		return null;
@@ -74,6 +82,28 @@ public class UserList extends JsonInfo {
 	private String email;
 
 	private boolean enabled;
+
+	private String salutation;
+
+	private String middlename;
+
+	private String title;
+
+	private String image;
+
+	private String address1;
+
+	private String address2;
+
+	private String city;
+
+	private String country;
+
+	private String phone;
+
+	private String sex;
+
+	private Date birthdate;
 
 	public UserList(Integer id, String username, String firstname, String lastname, String email, boolean enabled) {
 		this.setId(id);
@@ -93,25 +123,32 @@ public class UserList extends JsonInfo {
 						.iterator().next().getEmail() : null, user.isEnabled());
 	}
 
-	public UserList(Users user) {
-		this(user.getId(), user.getUsername(), user != null ? user.getFirstname() : null, user != null ? user
-				.getLastname() : null, null, user.isEnabled());
-	}
-
-	public String getFirstname() {
-		return firstname;
-	}
-
-	public void setFirstname(String firstname) {
+	public UserList(String firstname, String lastname, String middlename, String title, String salutation,
+			String image, String address1, String address2, String city, String country, String phone, String sex,
+			Date birthdate) {
 		this.firstname = firstname;
-	}
-
-	public String getLastname() {
-		return lastname;
-	}
-
-	public void setLastname(String lastname) {
 		this.lastname = lastname;
+		this.middlename = middlename;
+		this.title = title;
+		this.salutation = salutation;
+		this.image = image;
+		this.address1 = address1;
+		this.address2 = address2;
+		this.city = city;
+		this.country = country;
+		this.phone = phone;
+		this.sex = sex;
+		this.birthdate = birthdate;
+	}
+
+	public UserList(Users user) {
+		this(user.getFirstname(), user.getLastname(), user.getMiddlename(), user.getTitle(), user.getSalutation(), user
+				.getImage(), user.getAddress1(), user.getAddress2(), user.getCity(), user.getCountry(),
+				user.getPhone(), user.getSex(), user.getBirthdate());
+		setId(user.getId());
+		setName(user.getUsername());
+		setEnabled(user.isEnabled());
+		setEmail(user.getEmail());
 	}
 
 	public String getEmail() {
@@ -130,12 +167,119 @@ public class UserList extends JsonInfo {
 		this.enabled = enabled;
 	}
 
+	public String getSalutation() {
+		return salutation;
+	}
+
+	public void setSalutation(String salutation) {
+		this.salutation = salutation;
+	}
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	public String getMiddlename() {
+		return middlename;
+	}
+
+	public void setMiddlename(String middlename) {
+		this.middlename = middlename;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getImage() {
+		return image;
+	}
+
+	public void setImage(String image) {
+		this.image = image;
+	}
+
+	public String getAddress1() {
+		return address1;
+	}
+
+	public void setAddress1(String address1) {
+		this.address1 = address1;
+	}
+
+	public String getAddress2() {
+		return address2;
+	}
+
+	public void setAddress2(String address2) {
+		this.address2 = address2;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getSex() {
+		return sex;
+	}
+
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+
+	public Date getBirthdate() {
+		return birthdate;
+	}
+
+	public void setBirthdate(Date birthdate) {
+		this.birthdate = birthdate;
+	}
+
 	public String toJson() {
 		JSONSerializer serializer = new JSONSerializer();
 
 		serializer.exclude("*.class", "type");
-		serializer.include("id", "name", "firstname", "lastname", "email", "enabled");
+		serializer.include("id", "name", "firstname", "lastname", "middlename", "email", "enabled");
+		serializer.include("title", "salutation", "image", "address1", "address2", "city", "country", "phone", "sex",
+				"birthdate");
 
+		serializer.transform(DATE_TRANSFORMER2, Date.class);
 		serializer.transform(new FieldNameTransformer("username"), "name");
 
 		return "{\"data\":" + serializer.serialize(this) + "}";

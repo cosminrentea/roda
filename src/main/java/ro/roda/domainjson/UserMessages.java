7e5973c52fd5d14ebd0cca3085c28405b7294bf5
letitia <letitia.v@gmail.com>
2014-12-18 01:08:09 +0200

    Changed the structure of the field "messages" from userinfo, so that it corresponds to the one specified in issue #224. (Closes #224)

--- src/main/java/ro/roda/domainjson/UserMessages.java
+++ src/main/java/ro/roda/domainjson/UserMessages.java
@@ -9,6 +9,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Configurable;
 
 import ro.roda.domain.UserMessage;
+import ro.roda.transformer.FieldNameTransformer;
 import flexjson.JSONSerializer;
 
 @Configurable
@@ -18,9 +19,10 @@ public class UserMessages extends JsonInfo {
 		JSONSerializer serializer = new JSONSerializer();
 
 		serializer.exclude("*.class", "id", "name", "type");
-		serializer.include("timestamp", "subject", "direction", "read", "message");
+		serializer.include("timestamp", "fromid", "fromusername", "subject", "message");
 
 		serializer.transform(DATE_TRANSFORMER, Date.class);
+		serializer.transform(new FieldNameTransformer("datetime"), "timestamp");
 
 		return "{\"data\":" + serializer.serialize(collection) + "}";
 	}
@@ -34,20 +36,22 @@ public class UserMessages extends JsonInfo {
 			Iterator<UserMessage> incomingMessagesIterator = incomingMessages.iterator();
 			while (incomingMessagesIterator.hasNext()) {
 				UserMessage message = incomingMessagesIterator.next();
-				userMessages.add(new UserMessages(message.getTimestamp(), message.getSubject(), "incoming", message
-						.isRead(), message.getMessage()));
+				userMessages.add(new UserMessages(message.getTimestamp(), message.getSubject(), message.isRead(),
+						message.getMessage(), message.getFromUserId().getId(), message.getFromUserId().getUsername()));
 			}
 		}
 
-		List<UserMessage> outcomingMessages = findUserIncomingMessages(id);
-
-		if (outcomingMessages != null && outcomingMessages.size() > 0) {
-			Iterator<UserMessage> outcomingMessagesIterator = outcomingMessages.iterator();
-			while (outcomingMessagesIterator.hasNext()) {
-				UserMessage message = outcomingMessagesIterator.next();
-				userMessages.add(new UserMessages(null, null, "outcoming", false, message.getMessage()));
-			}
-		}
+		// List<UserMessage> outcomingMessages = findUserOutcomingMessages(id);
+		//
+		// if (outcomingMessages != null && outcomingMessages.size() > 0) {
+		// Iterator<UserMessage> outcomingMessagesIterator =
+		// outcomingMessages.iterator();
+		// while (outcomingMessagesIterator.hasNext()) {
+		// UserMessage message = outcomingMessagesIterator.next();
+		// userMessages.add(new UserMessages(null, null, "outcoming", false,
+		// message.getMessage()));
+		// }
+		// }
 
 		return userMessages;
 	}
@@ -67,23 +71,30 @@ public class UserMessages extends JsonInfo {
 
 	private String subject;
 
-	private String direction;
+	// private String direction;
 
 	private boolean read;
 
 	private String message;
 
-	public UserMessages(Date timestamp, String subject, String direction, boolean read, String message) {
+	private Integer fromid;
+
+	private String fromusername;
+
+	public UserMessages(Date timestamp, String subject, boolean read, String message, Integer fromid,
+			String fromusername) {
 		this.timestamp = timestamp;
 		this.subject = subject;
-		this.direction = direction;
+		// this.direction = direction;
 		this.read = read;
 		this.message = message;
+		this.fromid = fromid;
+		this.fromusername = fromusername;
 	}
 
 	public UserMessages(UserMessages userMessages) {
-		this(userMessages.getTimestamp(), userMessages.getSubject(), userMessages.getDirection(),
-				userMessages.isRead(), userMessages.getMessage());
+		this(userMessages.getTimestamp(), userMessages.getSubject(), userMessages.isRead(), userMessages.getMessage(),
+				userMessages.getFromid(), userMessages.getFromusername());
 	}
 
 	public Date getTimestamp() {
@@ -102,14 +113,6 @@ public class UserMessages extends JsonInfo {
 		this.subject = subject;
 	}
 
-	public String getDirection() {
-		return direction;
-	}
-
-	public void setDirection(String direction) {
-		this.direction = direction;
-	}
-
 	public boolean isRead() {
 		return read;
 	}
@@ -126,13 +129,30 @@ public class UserMessages extends JsonInfo {
 		this.message = message;
 	}
 
+	public Integer getFromid() {
+		return fromid;
+	}
+
+	public void setFromid(Integer fromid) {
+		this.fromid = fromid;
+	}
+
+	public String getFromusername() {
+		return fromusername;
+	}
+
+	public void setFromusername(String fromusername) {
+		this.fromusername = fromusername;
+	}
+
 	public String toJson() {
 		JSONSerializer serializer = new JSONSerializer();
 
 		serializer.exclude("*.class", "id", "name", "type");
-		serializer.include("timestamp", "subject", "direction", "read", "message");
+		serializer.include("timestamp", "fromid", "fromusername", "subject", "message");
 
 		serializer.transform(DATE_TRANSFORMER, Date.class);
+		serializer.transform(new FieldNameTransformer("datetime"), "timestamp");
 
 		return "{\"data\":" + serializer.serialize(this) + "}";
 	}
